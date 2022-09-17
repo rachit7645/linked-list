934d3f1e1aca7b7de918056c7419b61f2d7f05eb
Rachit Khandelwal <rachit7645@gmail.com>
2022-09-17 14:22:46 +0530

    Fix tests

--- tests/BasicNode.cpp
+++ tests/BasicNode.cpp
@@ -3,22 +3,6 @@
 using LinkedList::Node;
 using Tests::Result;
 
-template<typename T>
-void PrintNode(Node<T>* node)
-{
-	std::cout << "\nData: " << node->data << "\n";
-
-	if (node->prev != nullptr)
-	{
-		std::cout << "Prev: " << node->prev << "\n";
-	}
-
-	if (node->next != nullptr)
-	{
-		std::cout << "Next: " << node->next << "\n";
-	}
-}
-
 int main(UNUSED int argc, UNUSED char** argv)
 {
 	auto* first  = new Node<f32>();
@@ -29,9 +13,9 @@ int main(UNUSED int argc, UNUSED char** argv)
 	*second = {69.0f,  first,   third  };
 	*third  = {420.0f, second,  nullptr};
 
-	PrintNode(first);
-	PrintNode(second);
-	PrintNode(third);
+	Tests::PrintNode(first);
+	Tests::PrintNode(second);
+	Tests::PrintNode(third);
 
 	delete first;
 	delete second;
